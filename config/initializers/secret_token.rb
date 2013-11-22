e85114ed400ac371e0cb70988546f2fc11839d59
Vishal Vijay <0vishalvijay0@gmail.com>
2013-11-23 03:17:37 +0530

    initial commit fix

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Phonix::Application.config.secret_key_base = 'ae3e0d7fd18d43ac6023466ff1bb6bbe13e9dec81b7925c43757f4b06bae8f1c9651e5f817963fe52494e1c2f0664aa577bd09166ee0a92aa96231d90418b599'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Phonix::Application.config.session_store :cookie_store, key: '_phonix_session'
